aeae24b0cdbaa75444c88e0a6b9ca593a9940a0a
Saima Chaity <saima.s.chaity@gmail.com>
2021-03-05 13:58:42 -0800

    Created constants for currency options and initial json data

---
+++ client/src/constants/CurrencyOptions.js
@@ -0,0 +1,42 @@
+export const currency_options = [
+	{
+		key: "0",
+		value: "CAD",
+	},
+	{
+		key: "1",
+		value: "USD",
+	},
+	{
+		key: "2",
+		value: "NZD",
+	},
+	{
+		key: "3",
+		value: "AUD",
+	},
+	{
+		key: "4",
+		value: "SGD",
+	},
+	{
+		key: "5",
+		value: "EUR",
+	},
+	{
+		key: "6",
+		value: "CHF",
+	},
+	{
+		key: "7",
+		value: "BGN",
+	},
+	{
+		key: "8",
+		value: "BRL",
+	},
+	{
+		key: "9",
+		value: "GBP",
+	},
+];
