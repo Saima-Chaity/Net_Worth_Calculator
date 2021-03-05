5c1d1dffaa90716dabeb21c0bb1e8ae53b32a9e8
Saima Chaity <saima.s.chaity@gmail.com>
2021-03-05 13:56:20 -0800

    Created Landing page component and installed dependencies

---
+++ client/src/components/LandingPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class LandingPage extends Component{
+	constructor(props) {
+		super(props)
+		this.state = {}
+	}
+
+  render(){
+    return(
+			<div>
+				<span>Tracking your Networth</span>
+			</div>
+		)
+  }
+}
+
+export default LandingPage;
